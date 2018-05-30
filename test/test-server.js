47c752c7ae6b3d7ee05e03d31eed5d7435054050
Melissa Morales <melissa.fys@gmail.com>
2018-05-29 23:27:45 -0700

    updating test

--- test/test-server.js
+++ test/test-server.js
@@ -26,64 +26,61 @@ describe('Blog posts', function() {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
-        expect(res.body.length).to.be.at.least(1);
-        const expectedKeys = ['title', 'content', 'author'];
+        expect(res.body.length).to.be.above(0);
+        const expectedKeys = ['title', 'content', 'author','publishDate'];
         res.body.forEach(function(item) {
           expect(item).to.be.a('object');
-          expect(item).to.include.keys(expectedKeys);
+          expect(item).to.have.all.keys(expectedKeys);
         });
       });
   });
 
 
   it('should add a POST', function() {
-    const newItem = {title: 'Hellhole Canyon Trail', content: lorem(), author: 'MelMor'};
+    const newPost = {title: 'Hellhole Canyon Trail', content: lorem(), author: 'MelMor'};
+    const expectedKeys = ['id','publishDate'].concat(Object.keys(newpost));
+
     return chai.request(app)
       .post('/blog-post')
-      .send(newItem)
+      .send(newPost)
       .then(function(res) {
         expect(res).to.have.status(201);
         expect(res).to.be.json;
         expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('id', 'title', 'content', 'author');
-        expect(res.body.id).to.not.equal(null);
-        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+        expect(res.body).to.have.all.keys(expectedKeys);
+        expect(res.body.id).to.equal(newPost.title);
+        expect(res.body.id).to.equal(newPost.content);
+        expect(res.body.id).to.equal(newPost.author)
       });
   });
 
   it('should update posts on PUT', function() {
-    const updateData = {
-      name: 'Night Hiking',
-      content: lorem(),
-      author: 'Grant'
-    };
-
     return chai.request(app)
-      .get('/blog-post')
+      .get('/blog-posts')
       .then(function(res) {
-        updateData.id = res.body[0].id;
+        const updatedPost = Object.assign(res.body[0],{
+        	name: 'Night Hiking',
+      		content: lorem()
+      	});
         return chai.request(app)
-          .put(`/blog-post/${updateData.id}`)
-          .send(updateData);
-      })
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
-      });
-  });
+          .put(`/blog-post/${res.body[0].id}`)
+          .send(updatedPost);
+	      .then(function(res) {
+	       expect(res).to.have.status(200);
+	      });
+	    });
+	});
 
   it('should delete posts on DELETE', function() {
     return chai.request(app)
       // first have to get so we have an `id` of the post to delete
-      .get('/blog-post')
+      .get('/blog-posts')
       .then(function(res) {
         return chai.request(app)
-          .delete(`/blog-post/${res.body[0].id}`);
-      })
-      .then(function(res) {
+          .delete(`/blog-posts/${res.body[0].id}`);  
+          .then(function(res) {
         expect(res).to.have.status(204);
-      });
+    });
+   });
   });
 });
