05fa85e1b822ee603f4752ec7bae641ab96ec03b
Melissa Morales <melissa.fys@gmail.com>
2018-05-31 22:07:11 -0700

    testing v5

--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-
 const express = require('express');
 const morgan = require('morgan');
 
-const blogPostRouter = require('./blogPostRouter');
+const blogPostsRouter = require('./blogPostsRouter');
 const app = express();
 
-// log the http layer
 app.use(morgan('common'));
-//load the router and route
-app.use('/blog-post', blogPostRouter);
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+app.use('/blog-posts', blogPostsRouter);
+
+
+let server;
+
+// this function starts our server and returns a Promise.
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
--- test/test-server.js
+++ test/test-server.js
@@ -21,66 +21,83 @@ describe('Blog posts', function() {
 
   it('should list post on GET', function() {
     return chai.request(app)
-      .get('/blog-post')
+      .get('/blog-posts')
       .then(function(res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
         expect(res.body.length).to.be.above(0);
-        const expectedKeys = ['id', 'title', 'content', 'author','publishDate'];
         res.body.forEach(function(item) {
           expect(item).to.be.a('object');
-          expect(item).to.have.all.keys(expectedKeys);
+          expect(item).to.have.all.keys(
+            'id', 'title', 'content', 'author', 'publishDate')
         });
       });
   });
 
 
   it('should add a POST', function() {
-    const newPost = {title: 'Hellhole Canyon Trail', content: lorem(), author: 'MelMor'};
+    const newPost = {
+      title: 'Hellhole Canyon Trail', 
+      content: 'Is backed up against the San Ysidro Mountains just sw of the Anza-Borrego Desert State Park', 
+      author: 'MelMor'
+    };
     const expectedKeys = ['id','publishDate'].concat(Object.keys(newPost));
 
     return chai.request(app)
-      .post('/blog-post')
+      .post('/blog-posts')
       .send(newPost)
       .then(function(res) {
         expect(res).to.have.status(201);
         expect(res).to.be.json;
         expect(res.body).to.be.a('object');
         expect(res.body).to.have.all.keys(expectedKeys);
-        expect(res.body.id).to.equal(newPost.title);
-        expect(res.body.id).to.equal(newPost.content);
-        expect(res.body.id).to.equal(newPost.author)
+        expect(res.body.title).to.equal(newPost.title);
+        expect(res.body.content).to.equal(newPost.content);
+        expect(res.body.author).to.equal(newPost.author)
       });
   });
 
-  it('should update posts on PUT', function() {
+  it('should error if POST missing expected values', function() {
+    const badRequestData = {};
     return chai.request(app)
+      .post('/blog-posts')
+      .send(badRequestData)
+      .catch(function(res) {
+        expect(res).to.have.status(400);
+      });
+  });
+
+  it('should update blog posts on PUT', function() {
+
+    return chai.request(app)
+      // first have to get
       .get('/blog-posts')
-      .then(function(res) {
-        const updatedPost = Object.assign(res.body[0],{
-        	name: 'Night Hiking',
-      		content: lorem()
-      	});
+      .then(function( res) {
+        const updatedPost = Object.assign(res.body[0], {
+          title: 'connect the dots',
+          content: 'la la la la la'
+        });
         return chai.request(app)
-          .put(`/blog-post/${res.body[0].id}`)
+          .put(`/blog-posts/${res.body[0].id}`)
           .send(updatedPost)
-	      .then(function(res) {
-	       expect(res).to.have.status(204);
-	      });
-	    });
-	});
+          .then(function(res) {
+            expect(res).to.have.status(204);
+          });
+      });
+  });
 
   it('should delete posts on DELETE', function() {
     return chai.request(app)
-      // first have to get so we have an `id` of the post to delete
+      // first have to get
       .get('/blog-posts')
       .then(function(res) {
         return chai.request(app)
-          .delete(`/blog-posts/${res.body[0].id}`);  
+          .delete(`/blog-posts/${res.body[0].id}`)
           .then(function(res) {
-        expect(res).to.have.status(204);
-    });
-   });
+            expect(res).to.have.status(204);
+          });
+      });
   });
+
 });
