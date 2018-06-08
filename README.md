1baa1a4b291199eea4dcec7dd159ccdd8472c335
Melissa Morales <melissa.fys@gmail.com>
2018-06-08 03:34:59 -0700

    first commit

--- README.md
+++ README.md
@@ -1,13 +1,66 @@
-Challenge: Blog API
+Challenge: Add Mongoose to blogging app
 ================
 
-https://github.com/melishina/Challenge-Blog-API
+Create an API for a blogging app that has four endpoints:
+**GET /posts**
+- sends back all posts in the database.
+- each post should be an object that looks like this:
+```
+ {
+     "title": "some title",
+     "content": "a bunch of amazing words",
+     "author": "Sarah Clarke",
+     "created": "1481322758429"
+  }
+```
+- GET /posts/:id sends back a single post with :id if it exists, using the schema described above.
 
-Here are the requirements for this app:
-- Use npm init to initialize your project
-- Install uuid, which is a Node package that the blog post model will require, by running npm install uuid from within your folder for this project
-- Your app should support the four CRUD operations for a blog posts resource.
-- GET and POST requests should go to /blog-posts.
-- DELETE and PUT requests should go to /blog-posts/:id.
-- Use Express router and modularize routes to /blog-posts.
-- Add a couple of blog posts on server load so you'll automatically have some data to look at when the server starts.
+**POST /posts**
+- endpoint for creating new blog posts.
+- expects request body to contain a JSON object like this:
+```
+  {
+      "title": "some title",
+      "content": "a bunch of amazing words",
+      "author": {
+          "firstName": "Sarah",
+          "lastName": "Clarke"
+      }
+  }
+```
+- validates that the request body includes title, content, and author, and returns a 400 status and a helpful error message if one of these is missing.
+- it should return the new post (using the same key/value pairs as the posts returned by GET /posts).
+
+**PUT /posts/:id**
+- endpoint that allows you to update the title, content, and author fields.
+- expects request body to contain a JSON object like this (note that this would only update the title — if you wanted to update content or author, you'd have to send those over too):
+```
+  {
+      "id": "ajf9292kjf0",
+      "title": "New title"
+  }
+```
+- the id property in the request body must be there. If the id in the URL path (/posts/:id) and the one in the request body don't match, it should return a 400 status code with a helpful error message.
+- it should return the updated object, with a 200 status code.
+
+**DELETE /posts/:id**
+- allows you to delete a post with a given id.
+- responds with a 204 status code, but no content.
+
+**Additional requirements:**
+- Internal server errors: if any of the endpoints fail because of an internal server error, they should log the error and return a 500 status code along with a message like "Internal server error".
+- Seed data: Before you write a single line of code, download seed-data.json from this Gist. Then use mongoimport to seed your local database with some blog post data.
+- Use Git/Github: As always, use Git/Github to save and backup your work as you go.
+- Author in the db vs. the api: If you look at seed-data.json, you'll see that we seed our db with objects that look like this:
+```
+  {
+      "title": "some title",
+      "content": "a bunch of amazing words",
+      "author": {
+          "firstName": "Sarah",
+          "lastName": "Clarke"
+      }
+  }
+```
+This is also how the data should be stored in Mongo. However as previously described, the API does not return an object for the author property, but instead the author's first and last name separated by a space. Therefore, we recommend using an instance method like serialize from the restaurants app that you use to create the object the API should return. You might also consider creating a virtual property (perhaps called authorName) that returns the string value for author the API should return.
+- Deploy to Heroku: Once your app is working, deploy it to Heroku. Note that you'll have to set up a db on mLab as described earlier in this lesson. Seed the mLab database with the data from seed-data.json.
--- server.js
+++ server.js
@@ -1,45 +1,150 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose =  require ('mongoose');
+mongoose.Promise = global.Promise;
 
-const blogPostsRouter = require('./blogPostsRouter');
+const {BlogPosts} = require('./models');
+const {DATABASE_URL, PORT} = require ('./config');
+
 const app = express();
 
 app.use(morgan('common'));
+app.use(express.json());
 
-app.use('/blog-posts', blogPostsRouter);
+//GET
+app.get ('/posts',(request,response)=> { BlogPosts
+    .find()
+    .then(posts => {
+      response.json(posts.map(posts.serialize()));
+    })
+    .catch(e =>{
+      console.error(e);
+      response.status(500).json ({error: 'Something went wrong, Sorry'});
+    });
+  });
 
+app.get('/posts/:id', (request, response) => {
+  BlogPosts
+    .findById(request.params.id)
+    .then(post => response.json(post.serialize()))
+    .catch(e => {
+      console.error(e);
+      response.status(500).json({ error: 'something went wrong' });
+    });
+});
+
+//POST
+app.post('/posts', (request, response) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in request.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return response.status(400).send(message);
+    }
+  }
+
+  BlogPosts
+    .create({
+      title: request.body.title,
+      content: request.body.content,
+      author: request.body.author
+    })
+    .then(blogPosts => request.status(201).json(blogPosts.serialize()))
+    .catch(e => {
+      console.error(e);
+      response.status(500).json({ error: 'Something went wrong' });
+    });
+
+});
+
+//PUT
+app.put('/posts/:id', (request, response) => {
+  if (!(request.params.id && request.body.id && request.params.id === request.body.id)) {
+    response.status(400).json({
+      error: 'Request path id and request body id values dont match'
+    });
+  }
+
+  const updated = {};
+  const updateableFields = ['title', 'content', 'author'];
+  updateableFields.forEach(field => {
+    if (field in request.body) {
+      updated[field] = request.body[field];
+    }
+  });
+
+  BlogPosts
+    .findByIdAndUpdate(request.params.id, { $set: updated }, { new: true })
+    .then(updatedPost => response.status(204).end())
+    .catch(e => response.status(500).json({ message: 'Something went wrong' }));
+});
+
+//DELETE
+app.delete('/posts/:id', (request, response) => {
+  BlogPosts
+    .findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).json({ message: 'successfuly deleted' });
+    })
+    .catch(e => {
+      console.error(e);
+      response.status(500).json({ error: 'something went wrong' });
+    });
+});
+
+app.delete('/:id', (request, response) => {
+  BlogPosts
+    .findByIdAndRemove(request.params.id)
+    .then(() => {
+      console.log(`Deleted blog post with id \`${request.params.id}\``);
+      response.status(204).end();
+    });
+});
+
+app.use('*', function (request, response) {
+  response.status(404).json({ message: 'Not Found' });
+});
 
 let server;
 
-// this function starts our server and returns a Promise.
-function runServer() {
-  const port = process.env.PORT || 8080;
+// Start the server
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, e => {
+      if (e) {
+        return reject(e);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', e => {
+          mongoose.disconnect();
+          reject(e);
+        });
     });
   });
 }
 
+// Close the server
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(e => {
+        if (e) {
+          return reject(e);
+        }
+        resolve();
+      });
     });
   });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+  runServer(DATABASE_URL).catch(e => console.error(e));
+}
 
-module.exports = {app, runServer, closeServer};
+module.exports = { runServer, app, closeServer };
